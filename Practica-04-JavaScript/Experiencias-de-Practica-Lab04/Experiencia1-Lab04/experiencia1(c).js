6b6fd8fd447d998bb5a0690ef93bc9dd0cf3091c
Christian R. <joaquinromero24@hotmail.com>
2024-09-13 11:31:07 -0500

    Lab04: Cambio de URL en imagen

--- Practica-04-JavaScript/Experiencias-de-Practica-Lab04/Experiencia1-Lab04/experiencia1(c).js
+++ Practica-04-JavaScript/Experiencias-de-Practica-Lab04/Experiencia1-Lab04/experiencia1(c).js
@@ -3,7 +3,7 @@ const mensaje = document.getElementById('mensaje');
 
 function cambiarColorYMensaje() {
     // Cambiar el color de fondo
-    document.body.style.background = 'url("https://wallpapers.com/images/featured/sky-mvehfqz6w2ges2dj.jpg")';
+    document.body.style.background = 'url("https://github.com/Saico11/Desarrollo-de-Aplicaciones---04/blob/main/Practica-04-JavaScript/Experiencias-de-Practica-Lab04/Experiencia1-Lab04/wallpaper-sky.jpg?raw=true")';
     
     // Cambiar el mensaje
     mensaje.innerText = 'Irás al cielo!';
