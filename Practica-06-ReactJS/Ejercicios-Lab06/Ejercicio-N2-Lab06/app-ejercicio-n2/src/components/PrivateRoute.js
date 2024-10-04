86dd5e2142edc5d1795e7e4c15a455a160d7a5db
Christian R. <joaquinromero24@hotmail.com>
2024-10-04 17:24:03 -0500

    Lab06: Ejercicio 2 modificado
    - Cambios menores en los .css.
    - Se eliminaron los archivos que no tienen nada que ver con el ejercicio (PrivateRoute, TodoForm y Login).

--- Practica-06-ReactJS/Ejercicios-Lab06/Ejercicio-N2-Lab06/app-ejercicio-n2/src/components/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-// src/components/PrivateRoute.js
-import React from 'react';
-import { Navigate } from 'react-router-dom'; // Usamos Navigate en lugar de Redirect
-
-function PrivateRoute({ children, isAuthenticated }) {
-  return isAuthenticated ? children : <Navigate to="/login" />; // Si no está autenticado, redirige al login
-}
-
-export default PrivateRoute;
