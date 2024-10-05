383c1f6278ca850d557e5b6145ab42a4f1f2e611
Christian R. <joaquinromero24@hotmail.com>
2024-10-05 11:09:47 -0500

    Lab06: Ejercicio 1 Modificado
    - Se eliminó el src de copia porque ya no será necesario.
    - Se agregó el background para el ejercicio 1 (background-stars.gif).
    - Cambios menores.

--- Practica-06-ReactJS/Ejercicios-Lab06/Ejercicio-N1-Lab06/app-ejercicio-n1/src/components/HomePage.js
+++ Practica-06-ReactJS/Ejercicios-Lab06/Ejercicio-N1-Lab06/app-ejercicio-n1/src/components/HomePage.js
@@ -5,7 +5,7 @@ function HomePage() {
     <div>
       <h1>Bienvenido a la Aplicación de Experiencias de Práctica</h1>
       <p>Selecciona una experiencia desde la barra de navegación para comenzar.</p>
-      <img src="https://media.tenor.com/ZQaxe2gSk-4AAAAi/habbo-no-entry.gif"></img>
+      <img src="https://media.tenor.com/ZQaxe2gSk-4AAAAi/habbo-no-entry.gif" alt="gif-de-bienvenida"></img>
     </div>
   );
 }
