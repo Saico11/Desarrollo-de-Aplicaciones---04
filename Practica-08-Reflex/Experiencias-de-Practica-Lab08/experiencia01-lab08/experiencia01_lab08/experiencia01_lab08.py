98349c46735003eeb1d5c98cf0da4b35233abf6d
Christian R. <joaquinromero24@hotmail.com>
2024-10-18 19:45:46 -0500

    Lab08: Experiencia 1 Mejorada
    - Se añadieron mas cosas como centrar el contador, ponerle margenes, etc.
    - Tambien se añadio un rx.heading para poner el titulo de la experiencia y su descripción.
    - Cambios menores como mejoramiento de indentación.

--- Practica-08-Reflex/Experiencias-de-Practica-Lab08/experiencia01-lab08/experiencia01_lab08/experiencia01_lab08.py
+++ Practica-08-Reflex/Experiencias-de-Practica-Lab08/experiencia01-lab08/experiencia01_lab08/experiencia01_lab08.py
@@ -1,19 +1,7 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
-
 import reflex as rx
-
-from rxconfig import config
-
-
-class State(rx.State):
-    """The app state."""
-
-    ...
-
 
 class CountState(rx.State):
     count: int = 0
-
     def increment(self):
         self.count += 1
 
@@ -22,26 +10,36 @@ class CountState(rx.State):
 
 def counter():
     return rx.flex(
-        rx.button(
-            "Disminuir",
-            color_scheme="red",
-            on_click=CountState.decrement,
+        rx.vstack(
+            rx.heading("Experiencia 1: Contador Simple", font_size="24px", margin="10px"),
+            rx.box(
+                rx.button(
+                    "Disminuir",
+                    color_scheme="red",
+                    on_click=CountState.decrement,
+                ),
+                rx.heading(CountState.count, margin="10px"),
+                rx.button(
+                    "Incrementar",
+                    color_scheme="grass",
+                    on_click=CountState.increment,
+                ),
+                display="flex",
+                justify_content="center",
+                align_items="center",
+                width="100%"
+            ),
+            spacing="3",
         ),
-        rx.heading(CountState.count),
-        rx.button(
-            "Incrementar",
-            color_scheme="grass",
-            on_click=CountState.increment,
-        ),
-        spacing="3",
+        width="100%",
+        justify="center",
+        margin="5px"
     )
 
 def index() -> rx.Component:
     return rx.container(
         counter(),
     )
-
-
 
 app = rx.App()
 app.add_page(index)
