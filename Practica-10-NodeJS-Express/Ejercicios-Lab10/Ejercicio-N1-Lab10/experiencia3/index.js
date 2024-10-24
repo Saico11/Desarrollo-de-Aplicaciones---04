63d7bf13e4277d1b0151e7983a1b5d3cfa7a691a
RocKoGX <joaquinromero24@hotmail.com>
2024-10-24 13:27:40 -0500

    Lab10: Ejercicio 1 (Ejemplo 3 modificado)
    - Se tuvo que modificar el .js para que no saltara el error 404.
    - En el HTML se modifico la URL de la imagen para que solo haga referencia al de la imagen en la carpeta.

--- Practica-10-NodeJS-Express/Ejercicios-Lab10/Ejercicio-N1-Lab10/experiencia3/index.js
+++ Practica-10-NodeJS-Express/Ejercicios-Lab10/Ejercicio-N1-Lab10/experiencia3/index.js
@@ -1,7 +1,6 @@
-var http = require('http'),
-    fs = require('fs');
+var http = require('http');
+var fs = require('fs');
 
-// Cargar HTML, CSS e imagen
 var html = fs.readFileSync("./index.html");
 var css = fs.readFileSync("./styles.css");
 var img = fs.readFileSync("./bigcat.gif");
@@ -10,17 +9,20 @@ http.createServer(function(req, res) {
     if (req.url === '/styles.css') {
         res.writeHead(200, {'Content-Type': 'text/css'});
         res.write(css);
-    } else if(req.url === '/index.html'){
+        return res.end();
+    } else if(req.url === '/index.html' || req.url === '/') {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(html);
+        return res.end();
     } else if(req.url === '/bigcat.gif') {
         res.writeHead(200, {'Content-Type': 'image/gif'});
         res.write(img);
+        return res.end();
     } else {
         res.writeHead(404, {'Content-Type': 'text/plain'});
         res.write('404 Not Found');
+        return res.end();
     }
-    res.end();
 }).listen(3000, function() {
     console.log('Servidor corriendo en el puerto 3000');
 });
